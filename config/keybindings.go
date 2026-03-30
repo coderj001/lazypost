a76d1a3a8f00fb2813a196c7044d9fa9ffad28fe
coderj001 <amirajubolchi001@gmail.com>
2026-03-30 07:16:41 +0530

    feat: add key parsing and validation to keybindings config

--- config/keybindings.go
+++ config/keybindings.go
@@ -1,9 +1,12 @@
 package config
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/jroimartin/gocui"
 	"gopkg.in/yaml.v3"
 )
 
@@ -43,4 +46,58 @@ func DefaultKeybindings() *KeybindingConfig {
 			"switch-method": "ctrl-m",
 		},
 	}
+}
+
+func ParseKey(keyStr string) (gocui.Key, gocui.Modifier, error) {
+	keyStr = normalizeKeyString(keyStr)
+
+	switch keyStr {
+	case "ctrl-c":
+		return gocui.KeyCtrlC, gocui.ModNone, nil
+	case "ctrl-s":
+		return gocui.KeyCtrlS, gocui.ModNone, nil
+	case "ctrl-m":
+		return gocui.KeyCtrlM, gocui.ModNone, nil
+	case "tab":
+		return gocui.KeyTab, gocui.ModNone, nil
+	case "enter":
+		return gocui.KeyEnter, gocui.ModNone, nil
+	case "esc", "escape":
+		return gocui.KeyEsc, gocui.ModNone, nil
+	case "q":
+		return 'q', gocui.ModNone, nil
+	case ":":
+		return ':', gocui.ModNone, nil
+	default:
+		return 0, 0, fmt.Errorf("unknown key: %s", keyStr)
+	}
+}
+
+func normalizeKeyString(s string) string {
+	s = strings.ToLower(s)
+	s = strings.ReplaceAll(s, " ", "-")
+	return s
+}
+
+var validActions = map[string]bool{
+	"quit":          true,
+	"quit-alt":      true,
+	"next-view":     true,
+	"send-request":  true,
+	"start-editor":  true,
+	"switch-method": true,
+}
+
+func (c *KeybindingConfig) Validate() []string {
+	var warnings []string
+	for action, keyStr := range c.Keybindings {
+		if !validActions[action] {
+			warnings = append(warnings, fmt.Sprintf("unknown action '%s', skipping", action))
+			continue
+		}
+		if _, _, err := ParseKey(keyStr); err != nil {
+			warnings = append(warnings, fmt.Sprintf("invalid key '%s' for action '%s': %v", keyStr, action, err))
+		}
+	}
+	return warnings
 }
